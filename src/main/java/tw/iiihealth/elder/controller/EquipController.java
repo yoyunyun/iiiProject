2af949b658612e08cf537324f7b3979c1bcfb9cd
EthanChen0802 <u83kw4@gmail.com>
2021-08-28 12:41:36 +0800

    更新

--- src/main/java/tw/iiihealth/elder/controller/EquipController.java
+++ src/main/java/tw/iiihealth/elder/controller/EquipController.java
@@ -9,6 +9,8 @@ import java.util.Random;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -21,6 +23,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import tw.iiihealth.elder.model.Equip;
 import tw.iiihealth.elder.model.EquipService;
+import tw.iiihealth.membersystem.manager.model.Manager;
+import tw.iiihealth.membersystem.manager.service.ManagerService;
 
 
 
@@ -31,7 +35,8 @@ public class EquipController {
 	@Autowired
 	EquipService equipService;
 	
-	
+	@Autowired
+	private ManagerService managerService;
 
 	
 	
@@ -42,6 +47,12 @@ public class EquipController {
 		List<Equip> list = equipService.findAll();
 		
 		model.addAttribute("list", list);
+		
+		// 會員姓名顯示
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String manageraccount = auth.getName();
+		Manager user = managerService.searchUserDetails(manageraccount);
+		model.addAttribute("user", user);
 		
 		return "equip/equip-list";
 	}
@@ -54,6 +65,14 @@ public class EquipController {
 	public String QueryById(@RequestParam("eId") int eid, Model model) {
 		Equip equip = equipService.findById(eid);
 		model.addAttribute("equip", equip);
+		
+		// 會員姓名顯示
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String manageraccount = auth.getName();
+		Manager user = managerService.searchUserDetails(manageraccount);
+		model.addAttribute("user", user);
+		
+		
 		return "equip/equip-form";
 	}
 	
@@ -66,6 +85,12 @@ public class EquipController {
 		Equip equip = new Equip();
 		
 		model.addAttribute("equip", equip);
+		
+		// 會員姓名顯示
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String manageraccount = auth.getName();
+		Manager user = managerService.searchUserDetails(manageraccount);
+		model.addAttribute("user", user);
 		
 		return "equip/equip-form";
 	}
