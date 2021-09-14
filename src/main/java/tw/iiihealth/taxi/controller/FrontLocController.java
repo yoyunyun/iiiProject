cfc37c63a5ba9093270f9bff67f45056080ba0b2
yoyunyun <a515375a@gmail.com>
2021-09-14 10:05:53 +0800

    取消收藏

--- src/main/java/tw/iiihealth/taxi/controller/FrontLocController.java
+++ src/main/java/tw/iiihealth/taxi/controller/FrontLocController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import tw.iiihealth.membersystem.member.model.Member;
@@ -21,87 +22,112 @@ import tw.iiihealth.taxi.model.LocService;
 import tw.iiihealth.taxi.model.Location;
 
 @Controller
-@RequestMapping(path="/taxiFront")
+@RequestMapping(path = "/taxiFront")
 @Transactional
 public class FrontLocController {
 
 	@Autowired
 	private LocService locService;
-	
+
 	@Autowired
 	private MemberService memberService;
-	
+
 	@GetMapping(path = "/frontlocmainpage.controller")
-	public String processQueryLocMainPage() {		
+	public String processQueryLocMainPage() {
 		return "taxi/FrontSearchLoc";
 	}
-	
+
 	@GetMapping(path = "/booktaxi/frontloc.controller")
-	public String processQueryLocLogin() {		
+	public String processQueryLocLogin() {
 		return "taxi/FrontSearchLoc";
 	}
-	
+
 	@PostMapping(path = "/querylocall")
 	@ResponseBody
-	public List<Location> QueryLocAll(){
+	public List<Location> QueryLocAll() {
 		return locService.findAll();
 	}
-	
+
 	@PostMapping(path = "/queryloc/{cv}/{tv}/{tp}")
 	@ResponseBody
-	public List<Location> QueryLoc(@PathVariable("cv") String city, 
-			@PathVariable("tv") String town, @PathVariable("tp") String type){
-		
+	public List<Location> QueryLoc(@PathVariable("cv") String city, @PathVariable("tv") String town,
+			@PathVariable("tp") String type) {
+
 		if (town.equals("0")) {
-			return locService.queryCity(city,type);
-		}else {
-			return locService.queryTown(town,type);
+			return locService.queryCity(city, type);
+		} else {
+			return locService.queryTown(town, type);
 		}
 	}
-	
+
 	@GetMapping(path = "/search/{id}")
 	public String processSearchId(@PathVariable("id") int id, Model m) {
 
 		Location oldT = locService.findById(id);
 		String city = oldT.getCity();
 		String type = oldT.getType();
-		List<Location> listL = locService.queryCity(city,type);
-		
-		m.addAttribute("loc",oldT);
-		m.addAttribute("listLoc",listL);
+		List<Location> listL = locService.queryCity(city, type);
+
+		m.addAttribute("loc", oldT);
+		m.addAttribute("listLoc", listL);
 		return "taxi/LocIntro";
 	}
-	
+
 	@PostMapping(path = "/search2/{id}")
 	@ResponseBody
 	public Location processSearchId2(@PathVariable("id") int id, Model m) {
 		return locService.findById(id);
 	}
-	
-	/////////////	收藏店家  //////////////////
+
+	///////////// 收藏店家 //////////////////
 	// Ajax 會員收藏產品
-	@GetMapping(path="/booktaxi/collect/{lid}")
+	@GetMapping(path = "/booktaxi/collect/{lid}")
 	@ResponseBody
 	public String collect(@PathVariable("lid") int lid) {
-				
+
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		Member member = memberService.getCurrentlyLoggedInMember(auth);
-		
+
 		// 不能重複加一樣的產品
-		for (Location tempLoc:  member.getLocation()) {
+		for (Location tempLoc : member.getLocation()) {
 			if (tempLoc.getId() == lid) {
-					return "duplicate";
-				}
+				return "duplicate";
 			}
-				
+		}
+
 		// 沒有重複 查詢產品
-		Location loc= locService.findById(lid);
-		
+		Location loc = locService.findById(lid);
+
 		// 加到join table
 		member.addLoc(loc);
-			
+
 		return "success";
-		}
-	
-	
+	}
+
+	// 顯示會員的收藏店家
+	@GetMapping(path = "/booktaxi/loccollect")
+	public String showCollect(Model model) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		Member member = memberService.getCurrentlyLoggedInMember(auth);
+
+		List<Location> list = member.getLocation();
+		model.addAttribute("list", list);
+
+		return "taxi/LocCollection";
+	}
+
+	// 會員收藏刪除
+	@GetMapping(path = "/booktaxi/cancelcollect/{id}")
+	@ResponseBody
+	public List<Location> deletCollect(@PathVariable("id") int id) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		Member member = memberService.getCurrentlyLoggedInMember(auth);
+
+		Location loc = locService.findById(id);
+
+		member.removeLoc(loc);
+
+		return member.getLocation();
+	}
+
 }
